52f742be518552cc1f5216cd5d1641bdb19c70a6
Jlowzow <joachim@lowzow.org>
2017-04-19 19:40:59 +0200

    oppgave1.cpp

--- KompleksTemplate.cpp
+++ KompleksTemplate.cpp
@@ -15,20 +15,6 @@ void SkrivUt(Kompleks k){
 	cout << k.finnRealdel() << " + i" << k.finnImagdel() << endl;
 }
 
-void byttom(Kompleks &a, Kompleks &b){
-	Kompleks tmp = a;
-	a = b;
-	b = tmp;
-}
-
-void sorter(Kompleks * a, int n){
-	for( int i = 0; i < n; i++){
-		for(int j = i; j < n; j++){
-			if(a[i] < a[j]) byttom(a[i], a[j]);
-		}
-	}
-}
-
 int main(){
 
 	diverse<Kompleks> d;
